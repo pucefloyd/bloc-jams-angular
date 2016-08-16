5e49817f8aaed3e373368416e4ab40de8c6a893a
Your Namepucefloyd <pucefloyd>
2016-08-16 15:09:02 -0400

    Assignment 6

---
+++ app/scripts/controllers/CollectionCtrl.js
@@ -0,0 +1,12 @@
+(function() {
+	
+     function CollectionCtrl(Fixtures) {
+
+     	this.albums = Fixtures.getCollection(12);
+        
+     }
+ 
+     angular
+         .module('blocJams')
+         .controller('CollectionCtrl', CollectionCtrl);
+ })();
