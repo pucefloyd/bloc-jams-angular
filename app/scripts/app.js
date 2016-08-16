40d705210e4f2098bf0ac19165823f6becd8caf6
Your Namepucefloyd <pucefloyd>
2016-08-16 11:50:22 -0400

    checkpoint 1,2,3: completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,33 @@
-angular.module('blocJams', []);
+(function() {
+     function config($stateProvider, $locationProvider) {
+ 
+          $locationProvider
+         .html5Mode({
+             enabled: true,
+             requireBase: false
+         });
+
+}
+
+
+           $stateProvider
+         .state('landing', {
+             url: '/',
+             templateUrl: '/templates/landing.html'
+         })
+         .state('album', {
+             url: '/album',
+             templateUrl: '/templates/album.html'
+         });
+         .state('collection', {
+          		url: '/collection',
+          		controller: 'Collection.controller',
+          		templateUrl: '/templates/collection.html',
+      	 });
+
+ }
+
+   angular
+         .module('blocJams', ['ui.router'])
+         .config(config);
+ })();
